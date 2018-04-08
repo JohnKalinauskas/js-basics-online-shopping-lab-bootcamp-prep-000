a297101b81e2e553a825fa310a79a18030b5cd2a
John Kalinauskas <jkalinauskas@live.com>
2018-04-08 18:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-var cart = [];
-
-function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
-  return cart;
-}
-
 function addToCart(item) {
- // write your code here
-}
+  cart.push({ itemName : item , itemPrice : Math.floor((Math.random() * 100) + 1)});
 
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
+  return `${item} has been added to your cart.`;
 }
